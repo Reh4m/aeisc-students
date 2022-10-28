7d5d30161282fa3ce40226fb54d129c522652911
emmarp <ruiizemmanuel@gmai.com>
2022-10-28 06:44:08 -0500

    SigninAdmin query added

--- client/src/queries.js
+++ client/src/queries.js
@@ -1,6 +1,18 @@
 import { gql } from "apollo-boost";
 
 // Post Queries
+export const GET_CURRENT_ADMIN = gql`
+  query {
+    getCurrentAdmin {
+      _id
+      username
+      name
+      email
+      password
+    }
+  }
+`;
+
 export const GET_STUDENTS = gql`
   query {
     getStudents {
@@ -10,6 +22,14 @@ export const GET_STUDENTS = gql`
 `;
 
 // user Mutations
+export const SIGNIN_ADMIN = gql`
+  mutation($username: String!, $password: String!) {
+    signinAdmin(username: $username, password: $password) {
+      token
+    }
+  }
+`;
+
 export const SIGNUP_STUDENT = gql`
   mutation($controlNumber: String!) {
     signupStudent(controlNumber: $controlNumber) {
